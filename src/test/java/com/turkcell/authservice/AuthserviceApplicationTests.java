46f718b14cc7c5d193c1ccd82cd2c49b3d63520e
emreOytun <emreoytun58@gmail.com>
2024-08-29 22:17:06 +0300

    config class for test class added
    (cherry picked from commit f1d6947124c9f981b41750c0a24115bf506e5bde)

--- src/test/java/com/turkcell/authservice/AuthserviceApplicationTests.java
+++ src/test/java/com/turkcell/authservice/AuthserviceApplicationTests.java
@@ -3,7 +3,7 @@ package com.turkcell.authservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = AuthserviceApplication.class)
 class AuthserviceApplicationTests {
 
 	@Test
